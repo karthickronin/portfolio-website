785792359683dbcb7c065a5862e107e428c29f49
Your Name <you@example.com>
2025-01-29 10:27:00 +0530

    another commit

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,9 +1,5 @@
-import React from "react";
-import { BiLogoPostgresql } from "react-icons/bi";
-import { DiRedis } from "react-icons/di";
-import { FaNodeJs } from "react-icons/fa6";
-import { RiReactjsLine } from "react-icons/ri";
-import { SiMongodb } from "react-icons/si";
+import { FaNodeJs,  FaReact } from "react-icons/fa6";
+import { SiDjango, SiFlask, SiMongodb, SiMysql, SiPython } from "react-icons/si";
 import { TbBrandNextjs } from "react-icons/tb";
 import { motion } from "framer-motion";
 
@@ -36,23 +32,29 @@ const Technologies = () => {
         initial={{ x: -100, opacity: 0 }}
         transition={{ duration: 1.5 }}
         className="flex gap-4 flex-wrap items-center justify-center">
-        <motion.div initial="initial" animate="animate" variants={iconvariants(2.5)}>
-          <RiReactjsLine className="text-7xl text-cyan-400" />
+        <motion.div initial="initial" animate="animate" variants={iconvariants(2.5)}className="p-4">
+        <SiPython className="text-7xl text-yellow-500" />
+        </motion.div>
+        <motion.div initial="initial" animate="animate" variants={iconvariants(3.5)}className="p-4">
+        <SiDjango className="text-7xl text-green-800" />
+        </motion.div>
+        <motion.div initial="initial" animate="animate" variants={iconvariants(4.5)}className="p-4">
+          <FaReact className="text-7xl text-cyan-400" />
         </motion.div>
         <motion.div initial="initial" animate="animate" variants={iconvariants(3)} className="p-4">
           <TbBrandNextjs className="text-7xl" />
         </motion.div>
         <motion.div initial="initial" animate="animate" variants={iconvariants(5)} className="p-4">
-          <SiMongodb className="text-7xl text-cyan-500" />
+          <SiMongodb className="text-7xl text-green-500" />
         </motion.div>
         <motion.div initial="initial" animate="animate" variants={iconvariants(2)} className="p-4">
-          <DiRedis className="text-7xl text-red-700" />
+          <SiMysql className="text-7xl text-orange-500" />
         </motion.div>
         <motion.div initial="initial" animate="animate" variants={iconvariants(6)} className="p-4">
           <FaNodeJs className="text-7xl text-green-500" />
         </motion.div>
         <motion.div initial="initial" animate="animate" variants={iconvariants(4)} className="p-4">
-          <BiLogoPostgresql className="text-7xl text-sky-700" />
+          <SiFlask className="text-7xl text-white" />
         </motion.div>
       </motion.div>
     </div>
